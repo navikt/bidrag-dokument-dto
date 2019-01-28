334e5f2956715024407e1598b068beb580d355ce
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-01-28 15:36:20 +0100

    BD-307: saksnummer som streng

--- src/main/kotlin/no/nav/bidrag/dokument/dto/JournalpostDto.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/JournalpostDto.kt
@@ -17,7 +17,7 @@ data class JournalpostDto(
         @ApiModelProperty(value = "Dato dokument ble journalført") var journalfortDato: LocalDate? = null,
         @ApiModelProperty(value = "Identifikator av journalpost i midlertidig brevlager eller fra joark på formatet [BID|JOARK]-<journalpostId>") var journalpostId: String? = null,
         @ApiModelProperty(value = "Dato for når dokument er mottat, dvs. dato for journalføring eller skanning") var mottattDato: LocalDate? = null,
-        @ApiModelProperty(value = "Saksnummer på bidragssaken") var saksnummer: Long? = null,
+        @ApiModelProperty(value = "Saksnummer på bidragssaken") var saksnummer: String? = null,
         @ApiModelProperty(value = "Liste over saker hvor journalpostens sin gjelderBrukerId er representert (når det er en person)") var bidragssaker: List<BidragSakDto> = emptyList()
 )
 
