1cfeb24aa7d5256a0b4e5a14b05e1ae4480de93e
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-02-25 12:27:31 +0100

    not null id verdi for dto

--- src/main/kotlin/no/nav/bidrag/dokument/dto/EndreJournalpostCommandDto.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/EndreJournalpostCommandDto.kt
@@ -2,7 +2,10 @@ package no.nav.bidrag.dokument.dto
 
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
+import java.lang.Long.toHexString
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @ApiModel(value = "Metadata for endring av eksisterende journalpost")
 data class EndreJournalpostCommandDto(
@@ -12,7 +15,8 @@ data class EndreJournalpostCommandDto(
         @ApiModelProperty(value = "Kort oppsummert av journalført innhold") var beskrivelse: String? = null,
         @ApiModelProperty(value = "Fnr/dnr/bostnr eller orgnr for hvem/hva dokumentet gjelder") var gjelder: String? = null,
         @ApiModelProperty(value = "Dato dokument ble journalført") var journaldato: LocalDate? = null,
-        @ApiModelProperty(value = "Saksnummeret til tilknyttet bidragsak") var saksnummer: EndreSaksnummerDto? = null
+        @ApiModelProperty(value = "Saksnummeret til tilknyttet bidragsak") var saksnummer: EndreSaksnummerDto? = null,
+        @ApiModelProperty(value = "Id for dtoen") var dtoId: String = toHexString(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
 )
 
 @ApiModel(value = "Metadata for endring av saksnummer på en eksisterende journalpost")
