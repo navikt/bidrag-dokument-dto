c87737c9b845ce6a3e7fb3ca9a73b934ab9002ca
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-04-29 11:03:54 +0200

    endring av dokumentdato

--- src/main/kotlin/no/nav/bidrag/dokument/dto/EndreJournalpostCommandDto.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/EndreJournalpostCommandDto.kt
@@ -9,6 +9,7 @@ data class EndreJournalpostCommandDto(
         @ApiModelProperty(value = "Identifikator av journalpost") var journalpostId: String? = null,
         @ApiModelProperty(value = "Avsenders navn (med eventuelt fornavn bak komma)") var avsenderNavn: String? = null,
         @ApiModelProperty(value = "Kort oppsummert av journalført innhold") var beskrivelse: String? = null,
+        @ApiModelProperty(value = "Dato for dokument i journalpost") var dokumentDato: LocalDate? = null,
         @ApiModelProperty(value = "Fnr/dnr/bostnr eller orgnr for hvem/hva dokumentet gjelder") var gjelder: String? = null,
         @ApiModelProperty(value = "Dato dokument ble journalført") var journaldato: LocalDate? = null,
         @ApiModelProperty(value = "Saksnummeret til tilknyttet bidragsak") var saksnummer: EndreSaksnummerDto? = null
