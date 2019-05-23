a3d8891b5c79f70b90d8691f24a8a8ff9f7a8e12
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-05-23 09:05:00 +0200

    fjernet feltene identType og aktorType
    - forenklet if logikk og la til utredet identtype i feilmelding

--- src/main/kotlin/no/nav/bidrag/dokument/dto/AktorDto.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/AktorDto.kt
@@ -8,24 +8,18 @@ import java.util.Optional
 @ApiModel(value = "Metadata om en aktør")
 data class AktorDto(
         @ApiModelProperty(value = "Identifaktor til aktøren") var ident: String,
-        @ApiModelProperty(value = "Identtypen til identen (bnr, NorskIdent eller orgnr)") var identType: String,
-        @ApiModelProperty(value = "Aktørtype (person eller organisasjon)") val aktorType: String,
         @ApiModelProperty(value = "Personinformasjon dersom aktør er en person") val personinfo: PersonDto?
 ) {
-    constructor(ident: String) : this(ident, "ukjent", "ukjent", null)
-
-    @Suppress("unused") // used by java code
-    constructor(ident: String, identType: String) : this(ident, identType, "ukjent", null)
+    constructor(ident: String) : this(ident, null)
 
     fun fetchIdentType(): String {
         if (erPerson()) return personIdentType()
-        if (erIdentMedSiffer() && ident.length == 9) return "orgnr"
+        if (erIdentMedSiffer()) return "orgnr"
 
         return "ukjent"
     }
 
     fun fetchPersonIdentType(): Optional<String> {
-        if (aktorType == "organisasjon") return Optional.empty()
         if (erIkkeIdentMedSiffer()) return Optional.empty()
         if (erIkkeLengdenPaIdentenElleveTegn()) return Optional.of("AKTOERID")
 
@@ -48,7 +42,7 @@ data class AktorDto(
     private fun erIkkeIdentMedElleveSiffer() = ident.length != 11 || !ident.matches("[0-9]*".toRegex())
 
     private fun throwUkjentIdentType(): Nothing {
-        throw IllegalStateException("Ukjent ident ($ident) for $aktorType")
+        throw IllegalStateException("Ukjent ident ($ident): utredet identtype er ${fetchIdentType()}.")
     }
 
     fun erPerson(): Boolean {
