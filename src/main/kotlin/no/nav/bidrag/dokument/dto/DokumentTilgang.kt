605a85bd48b6c8e18bacccf14f71173601543de2
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-06-18 10:12:21 +0200

    DokumentTilgang request/response med felter som ikke er null

--- src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentTilgang.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentTilgang.kt
@@ -3,14 +3,13 @@ package no.nav.bidrag.dokument.dto
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
-@ApiModel(value = "Metadata for en url til et fysisk dokument")
-data class DokumentUrlDto(
-        @ApiModelProperty(value = "url til et fysisk dokument") var dokumentUrl: String? = null,
-        @ApiModelProperty(value = "type system som er ansvarlig for dokumentet, eks: BREVLAGER") var type: String? = null
-)
-
 @ApiModel(value = "Metadata for en forespørsel om tilgang til et dokument")
 data class DokumentTilgangRequest(
-        @ApiModelProperty(value = "referansen til dokumentet det ønskes tilgang til") var dokumentReferanse: String? = null,
-        @ApiModelProperty(value = "saksbehandleren som ønsker tilgang") var saksbehandler: String? = null
+        @ApiModelProperty(value = "referansen til dokumentet det ønskes tilgang til") var dokumentReferanse: String
+)
+
+@ApiModel(value = "Metadata for en url til et fysisk dokument")
+data class DokumentTilgangResponse(
+        @ApiModelProperty(value = "url til et fysisk dokument") var dokumentUrl: String,
+        @ApiModelProperty(value = "type system som er ansvarlig for dokumentet, eks: BREVLAGER") var type: String
 )
