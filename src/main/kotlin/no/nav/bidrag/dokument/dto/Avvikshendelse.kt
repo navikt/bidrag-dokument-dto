6c48881091f5d3b8871184101ddc9ee4a103bb33
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-06-25 10:14:13 +0200

    OpprettAvvikshendelseResponse

--- src/main/kotlin/no/nav/bidrag/dokument/dto/Avvikshendelse.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/Avvikshendelse.kt
@@ -11,6 +11,14 @@ data class Avvikshendelse(
     constructor(avvikType: AvvikType) : this(avvikType = avvikType.name)
 }
 
+data class OpprettAvvikshendelseResponse(
+        @ApiModelProperty(value = "Type avvik") val avvikType: String,
+        @ApiModelProperty(value = "Beskrivelse av opprettet avvik") var beskrivelse: String? = null
+) {
+    constructor() : this("avvik ikke angitt")
+    constructor(avvikType: AvvikType) : this(avvikType = avvikType.name)
+}
+
 enum class AvvikType {
     BESTILL_ORGINAL
 }
