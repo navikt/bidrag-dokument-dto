79fa12e58f1916a8afa4e8fb2b4b2da074cfd4b2
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-20 16:22:47 +0100

    slettet NyJournalpostCommand

--- src/main/kotlin/no/nav/bidrag/dokument/dto/Journalpost.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/Journalpost.kt
@@ -35,32 +35,6 @@ data class DokumentDto(
         @ApiModelProperty(value = "Kort oppsummert av journalført innhold") var tittel: String? = null
 )
 
-@ApiModel(value = "Metadata for registrering av ny journalpost")
-data class NyJournalpostCommand(
-        @ApiModelProperty(value = "Avsenders navn (med eventuelt fornavn bak komma)") var avsenderNavn: String? = null,
-        @ApiModelProperty(value = "Kort oppsummert av journalført innhold") var beskrivelse: String? = null,
-        @ApiModelProperty(value = "Dato for dokument i journalpost") var dokumentdato: LocalDate? = null,
-        @ApiModelProperty(value = "Dokumentreferanse for det arkiverte dokumentet") var dokumentreferanse: String? = null,
-        @ApiModelProperty(value = "Inngående (I), utgående (U) dokument, (X) internt notat") var dokumentType: String? = null,
-        @ApiModelProperty(value = "Fnr/dnr/bostnr eller orgnr for hvem/hva dokumentet gjelder") var gjelder: String? = null,
-        @ApiModelProperty(value = "Dato dokument ble journalført") var journaldato: LocalDate? = null,
-        @ApiModelProperty(value = "Enhetsnummer hvor journalføring ble gjort") var journalforendeEnhet: String? = null,
-        @ApiModelProperty(value = "Saksbehandler som var journalfører") var journalfortAv: String? = null,
-        @ApiModelProperty(value = "Dato for når dokument er mottat, dvs. dato for journalføring eller skanning") var mottattDato: LocalDate? = LocalDate.now(),
-        @ApiModelProperty(value = "Saksnummeret til tilknyttet bidragsak") var saksnummer: String? = null,
-        @ApiModelProperty(value = "Fagområde journalposten tilhører for bidragssaker. 'BNR' = bidrag, 'FAR' = farskap") var fagomrade: String? = null,
-        @ApiModelProperty(value = "Behandlingstema") var behandlingstema: String? = null,
-        @ApiModelProperty(value = "Type ident for gjelder: FNR, ORGNR, AKTOERID") var gjelderType: String? = null,
-        @ApiModelProperty(value = "Tittel på journalposten") var tittel: String? = null,
-        @ApiModelProperty(value = "En liste over endringer i dokumenter") var dokumenter: List<OpprettDokument> = emptyList()
-)
-
-data class OpprettDokument (
-        @ApiModelProperty(value = "Brevkoden til dokumentet") var brevkode: String? = null,
-        @ApiModelProperty(value = "Dokumentets kategori, for eksempel SOK (soknad), SED eller FORVALTNINGSNOTAT") var dokumentKategori: String? = null,
-        @ApiModelProperty(value = "Tittel på dokumentet") var tittel: String? = null
-)
-
 @ApiModel(value = "Metadata for endring av eksisterende journalpost")
 data class EndreJournalpostCommand(
         @ApiModelProperty(value = "Identifikator av journalpost") var journalpostId: String? = null,
