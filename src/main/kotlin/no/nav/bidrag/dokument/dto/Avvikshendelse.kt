b2eb55658d0596f9a78f5506d70bcd0033b56bc9
Jan Kjetil Skår <43172034+s148719@users.noreply.github.com>
2020-08-05 13:43:23 +0200

    BD-1063: Omstrukturerer OpprettAvvikshendelseResponse. Ny avvikType A… (#142)
    * BD-1063: Omstrukturerer OpprettAvvikshendelseResponse. Ny avvikType ARKIVERE_JOURNALPOST. Ny Avvikshendelse-konstruktør.

--- src/main/kotlin/no/nav/bidrag/dokument/dto/Avvikshendelse.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/Avvikshendelse.kt
@@ -12,11 +12,17 @@ data class Avvikshendelse(
         @ApiModelProperty(value = "Eventuelle detaljer som skal følge avviket") var detaljer: Map<String, String> = HashMap(),
         @ApiModelProperty(value = "Saksnummer til sak når journalpost er journalført") var saksnummer: String?
 ) {
-    constructor() : this(avvikType = "avvik ikke angitt", enhetsnummer = null, beskrivelse = null, saksnummer = null)
-    constructor(avvikType: String, enhetsnummer: String?) : this(avvikType, enhetsnummer, beskrivelse = null, saksnummer = null)
-    constructor(avvikType: String, enhetsnummer: String?, saksnummer: String?) : this(
-            avvikType, enhetsnummer, beskrivelse = null, saksnummer = saksnummer
-    )
+    constructor()
+            : this(avvikType = "avvik ikke angitt", enhetsnummer = null, beskrivelse = null, saksnummer = null)
+
+    constructor(avvikType: String, enhetsnummer: String?)
+            : this(avvikType, enhetsnummer, beskrivelse = null, saksnummer = null)
+
+    constructor(avvikType: String, enhetsnummer: String?, detaljer: Map<String, String>, saksnummer: String?)
+            : this(avvikType, enhetsnummer, null, detaljer, saksnummer)
+
+    constructor(avvikType: String, enhetsnummer: String?, saksnummer: String?)
+            : this(avvikType, enhetsnummer, beskrivelse = null, saksnummer = saksnummer)
 
     fun hent(): Optional<AvvikType> {
         try {
@@ -27,19 +33,25 @@ data class Avvikshendelse(
     }
 }
 
-@ApiModel(value = "Responsen til en avvikshendelse")
-data class OpprettAvvikshendelseResponse(
-        @ApiModelProperty(value = "Type avvik") var avvikType: String,
+interface OpprettAvvikshendelseResponse {
+    var avvikType: String
+}
+
+@ApiModel(value = "Responsen til en avvikshendelse med oppgave")
+data class OpprettAvvikshendelseOppgaveResponse(
+        override @ApiModelProperty(value = "Type avvik") var avvikType: String,
         @ApiModelProperty(value = "Oppgave id for oppgaven som ble opprettet på bakgrunn av avviket") var oppgaveId: Long? = null,
         @ApiModelProperty(value = "Enhetsnummer til enheten som oppgaven er tildelt") var tildeltEnhetsnr: String? = null,
         @ApiModelProperty(value = "Oppgavens tema") var tema: String? = null,
         @ApiModelProperty(value = "Oppgavens type") var oppgavetype: String? = null
-) {
+) : OpprettAvvikshendelseResponse {
     constructor() : this("avvik ikke angitt")
     constructor(avvikType: AvvikType) : this(avvikType.name)
 }
 
+
 enum class AvvikType {
+    ARKIVERE_JOURNALPOST,
     BESTILL_ORIGINAL,
     BESTILL_RESKANNING,
     BESTILL_SPLITTING,
