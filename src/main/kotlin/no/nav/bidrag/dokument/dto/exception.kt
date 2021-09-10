0503126d18969eadca0c8bc6073af25850b78959
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-09-10 12:48:53 +0200

    exceptions for bruk i bidrag.dokument
    - JournalpostBeskrivelseException
    - JournalpostHendelseException
    - OppgaveException
    - TokenException

--- src/main/kotlin/no/nav/bidrag/dokument/dto/exception.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/exception.kt
@@ -1,4 +1,11 @@
 package no.nav.bidrag.dokument.dto
 
+class JournalpostBeskrivelseException(message: String) : RuntimeException(message)
+class JournalpostHendelseException(message: String, throwable: Throwable?) : RuntimeException(message, throwable) {
+    constructor(message: String) : this(message = message, throwable = null)
+}
+
 class JournalpostIkkeFunnetException(message: String) : RuntimeException(message)
+class OppgaveException(message: String) : RuntimeException(message)
 class OppgaveIkkeOpprettetException(message: String) : RuntimeException(message)
+class TokenException(message: String) : RuntimeException(message)
