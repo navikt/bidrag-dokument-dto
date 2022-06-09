452954cb67d9c1a407b5966ffc95675a6dc88173
Ugur Alpay Cenar <cenar93@gmail.com>
2022-06-09 15:55:37 +0200

    Journalposthendelse add parameter fnr (#209)
    * Add fnr to JournalpostHendelse
    
    * Fix test

--- src/main/kotlin/no/nav/bidrag/dokument/dto/JournalpostHendelse.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/JournalpostHendelse.kt
@@ -3,12 +3,20 @@ package no.nav.bidrag.dokument.dto
 data class JournalpostHendelse(
     var journalpostId: String = "na",
     var aktorId: String? = null,
+    var fnr: String? = null,
     var fagomrade: String? = null,
     var enhet: String? = null,
     var journalstatus: String? = null,
     var sporing: Sporingsdata? = null
 ) {
 
+    internal fun harEnhet() = enhet != null
+    internal fun harAktorId() = aktorId != null
+    internal fun harJournalpostIdPrefix() = journalpostId.contains("-")
+    internal fun erBidragJournalpost() = harJournalpostIdPrefix() && journalpostId.startsWith("BID")
+    internal fun erJoarkJournalpost() = harJournalpostIdPrefix() && journalpostId.startsWith("JOARK")
+    internal fun hentEndretAvEnhetsnummer() = if (sporing?.enhetsnummer != null) sporing!!.enhetsnummer else enhet
+    internal fun hentSaksbehandlerInfo() = if (sporing != null) sporing!!.lagSaksbehandlerInfo() else "ukjent saksbehandler"
     constructor(
         journalpostId: String?,
         enhet: String?,
@@ -32,4 +40,7 @@ data class Sporingsdata(
     var brukerident: String? = null,
     var saksbehandlersNavn: String? = null,
     var enhetsnummer: String? = null
-)
+){
+    internal fun lagSaksbehandlerInfo() = if (brukerident == null) "ukjent saksbehandler" else hentBrukeridentMedSaksbehandler()
+    private fun hentBrukeridentMedSaksbehandler() = if (saksbehandlersNavn == null) brukerident!! else "$brukerident, $saksbehandlersNavn"
+}
