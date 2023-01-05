dff89b8e591702df90b66f9b0c5358df1daa97f1
Ugur Cenar <cenar93@gmail.com>
2023-01-05 14:57:20 +0100

    Lagt til ÅpneDokumentMetadata data objekt

--- src/main/kotlin/no/nav/bidrag/dokument/dto/ÅpneDokumentMetadata.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/ÅpneDokumentMetadata.kt
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 data class ÅpneDokumentMetadata(
     @Schema(description = "Link for å åpne dokumentet") val link: String? = null,
+    val journalpostId: String? = null,
+    val dokumentreferanse: String? = null,
     @Schema(description = "Hvilken format dokument er på") val format: DokumentFormatDto,
     @Schema(description = "Status på dokumentet")  val status: DokumentStatusDto
 )
