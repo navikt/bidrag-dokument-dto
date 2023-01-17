cc6b3e303376464dfec9719be3f67ca5a77955d5
Ugur Cenar <cenar93@gmail.com>
2023-01-17 08:46:08 +0100

    Endret ÅpneDokumentMetadata til DokumentMetadata

--- src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentMetadata.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentMetadata.kt
@@ -2,10 +2,10 @@ package no.nav.bidrag.dokument.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-data class ÅpneDokumentMetadata(
+data class DokumentMetadata(
     @Schema(description = "Journalpostid med arkiv prefiks som skal benyttes når dokumentet hentes") val journalpostId: String? = null,
     val dokumentreferanse: String? = null,
-    @Schema(description = "Hvilken format dokument er på. Dette forteller hvordan dokumentet bør åpnes.") val format: DokumentFormatDto,
+    @Schema(description = "Hvilken format dokument er på. Dette forteller hvordan dokumentet må åpnes.") val format: DokumentFormatDto,
     @Schema(description = "Status på dokumentet") val status: DokumentStatusDto,
     @Schema(description = "Hvilken arkivsystem dokumentet er lagret på") val arkivsystem: DokumentArkivSystemDto
 )
