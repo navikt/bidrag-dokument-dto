5f47fa05a353bafb0b7c5fdf2c03669d97f30371
Ugur Cenar <cenar93@gmail.com>
2023-03-31 11:28:43 +0200

    DokumentMetadata lagt til tittel parameter

--- src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentMetadata.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/DokumentMetadata.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class DokumentMetadata(
     @Schema(description = "Journalpostid med arkiv prefiks som skal benyttes når dokumentet hentes") val journalpostId: String? = null,
     val dokumentreferanse: String? = null,
+    val tittel: String? = null,
     @Schema(description = "Hvilken format dokument er på. Dette forteller hvordan dokumentet må åpnes.") val format: DokumentFormatDto,
     @Schema(description = "Status på dokumentet") val status: DokumentStatusDto,
     @Schema(description = "Hvilken arkivsystem dokumentet er lagret på") val arkivsystem: DokumentArkivSystemDto
--- src/main/kotlin/no/nav/bidrag/dokument/dto/constants.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/dto/constants.kt
@@ -1,3 +1,8 @@
 package no.nav.bidrag.dokument.dto
 
 const val FARSKAP_UTELUKKET_PREFIKS = "FARSKAP UTELUKKET"
+
+object BidragEnhet {
+    const val ENHET_FARSKAP = "4860"
+    const val ENHET_UTLAND = "4865"
+}
